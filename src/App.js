855f40ff27753c1676f67200b31bef3216c1e779
parogracz <dnasser113@gmail.com>
2021-04-03 17:53:19 +0200

    Pierwszy projest React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
+function BR(){
+  return(
+    <div className="Clear"> </div>
+  );
+}
+function Button_Num(value){
+  return (
+    <div className="Button_1" >
+      <button className="Button Button_Num">
+        {value}
+      </button>
+    </div>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div className="App">
+        <div className="Screen">
+          
+        </div>
+        <div className="Numbers">
+          <div className="row">
+            {Button_Num(1)}
+            {Button_Num(2)}
+            {Button_Num(3)}
+          </div>
+          {BR()}
+          <div className="row">
+            {Button_Num(4)}
+            {Button_Num(5)}
+            {Button_Num(6)}
+          </div>
+          {BR()}
+          <div className="row">
+            {Button_Num(7)}
+            {Button_Num(8)}
+            {Button_Num(9)}
+          </div>
+          {BR()}
+        </div>
+        <div className="Function">
+
+        </div>
+      </div>
+      
     </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Text, ScrollView} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <App/>
+  </React.StrictMode>, 
   document.getElementById('root')
 );
 
