047e46a59a747bd4bae2008cc9348109cd8f9280
parogracz <dnasser113@gmail.com>
2021-04-09 01:30:19 +0200

    Poprawka Wyświetlenia

--- src/App.js
+++ src/App.js
@@ -3,51 +3,67 @@ import './App.css';
 import React,{useState} from 'react';
 
 function BR(){
-  return(
-    <div className="Clear"> </div>
-  );
+return(
+  <div className="Clear"> </div>
+);
 }
-function Button_Num(value){
+
+function App() {
+  const [i,setI] = useState("");
+  function Button_Num(value){
   return (
-    <div className="Button_1" >
+    <div className="Button_1" 
+      onClick={() => {setI(i+value)}}
+    >
       <button className="Button Button_Num">
         {value}
       </button>
     </div>
-  );
-}
-function App() {
+    );
+  }
+  function Screen(num) {
+  return(
+    <div>
+      {num}
+    </div>
+    );
+  }
+
   return (
     <div className="Container">
       <div className="App">
         <div className="Screen">
-          
+          {i}
         </div>
         <div className="Numbers">
           <div className="row">
-            {Button_Num(1)}
-            {Button_Num(2)}
-            {Button_Num(3)}
+            {Button_Num('1')}
+            {Button_Num('2')}
+            {Button_Num('3')}
           </div>
           {BR()}
           <div className="row">
-            {Button_Num(4)}
-            {Button_Num(5)}
-            {Button_Num(6)}
+            {Button_Num('4')}
+            {Button_Num('5')}
+            {Button_Num('6')}
           </div>
           {BR()}
           <div className="row">
-            {Button_Num(7)}
-            {Button_Num(8)}
-            {Button_Num(9)}
+            {Button_Num('7')}
+            {Button_Num('8')}
+            {Button_Num('9')}
           </div>
           {BR()}
+          <div className="row">
+            {Button_Num('C')}
+            {Button_Num('0')}
+            {Button_Num('<-')}
+          </div>
         </div>
         <div className="Function">
 
         </div>
       </div>
-      
     </div>
     
   );
